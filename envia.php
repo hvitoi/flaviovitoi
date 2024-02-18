c630ae9aa732f032ce04642714bd42b92ab65a4c
Henrique Vitoi <44772383+hvitoi@users.noreply.github.com>
2024-02-17 21:26:12 -0300

    Formatting & Disable http endpoints

--- envia.php
+++ envia.php
@@ -1,21 +1,24 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!--Site produzido por Henrique Abrantes Vitoi - hvitoi@gmail.com-->
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Fl&aacute;vio Vitoi</title>
-		<link rel="stylesheet" type="text/css" href="style.css"/> 
-	</head>
-	
-	<body>
-		<table align="center" border="0" width="1200">
-		
-			<tr><td>
+
+<head>
+	<title>Fl&aacute;vio Vitoi</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+	<table align="center" border="0" width="1200">
+
+		<tr>
+			<td>
 				<div align="center">
-					<img src="logo.jpg"/>
+					<img src="logo.jpg" />
 				</div>
-			</td></tr>
-				
-			<tr><td> 
+			</td>
+		</tr>
+
+		<tr>
+			<td>
 				<h2 class="menu">
 					<div id="menu">
 						<ul>
@@ -27,9 +30,11 @@
 						</ul>
 					</div>
 				</h2>
-			</td></tr>
-				
-			<tr><td>
+			</td>
+		</tr>
+
+		<tr>
+			<td>
 				<?php
 					$to = "gerais22@hotmail.com";
 					$subject = "Mensagem do Site";
@@ -41,14 +46,17 @@
 					mail($to,$subject,$message,$headers);
 					echo "<p>E-mail enviado. Por favor, aguarde a resposta em sua caixa de entrada.</p>";
 				?>
-			</td></tr>
+			</td>
+		</tr>
 
-			<tr><td>
+		<tr>
+			<td>
 				<hr size="3" noshade="noshade" color=#000000 />
 				<div align="center"><img src="assinatura.gif"></p>
-			</td></tr>
-			
-		</table>
-	</body>
+			</td>
+		</tr>
+
+	</table>
+</body>
 
 </html>
